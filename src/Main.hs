f631a3b7799aac24d2499e02e73238bcbefbaacc
Sean Seefried <sean.seefried@gmail.com>
2015-10-12 08:10:45 +1000

    Add minimal example

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Data.BKTree
+import Text.EditDistance
+import Text.Printf
+
+defaultCosts :: EditCosts
+defaultCosts = EditCosts one one one one
+  where one = ConstantCost 1
+
+dist :: String -> String -> Int
+dist =  levenshteinDistance defaultCosts
+
+emptyTree = empty dist
+
 main :: IO ()
-main = return ()
+main = do
+  let s1 = "sean"
+      s2 = "sux"
+  printf "Distance: %s %s = %d\n" s1 s2 (dist s1 s2)
+  return ()
